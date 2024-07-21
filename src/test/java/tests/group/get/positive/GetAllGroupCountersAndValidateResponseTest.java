2210119c1c041e1aecc3f351110a91ca65957468
boris <borissiobko@gmail.com>
2024-07-22 00:56:16 +0300

    Implement test on getting all public counters and checking amount of them and validate each of them

--- src/test/java/tests/group/get/positive/GetAllGroupCountersAndValidateResponseTest.java
+++ src/test/java/tests/group/get/positive/GetAllGroupCountersAndValidateResponseTest.java
@@ -2,14 +2,14 @@ package tests.group.get.positive;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tests.ApiTest;
 import utils.Endpoints;
 import utils.GroupMethodsUri;
 
+import java.util.Arrays;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
@@ -19,24 +19,24 @@ import static utils.Specifications.requestSpec;
 import static utils.Specifications.responseSpecOK200;
 
 /**
- * Тест, который получает определенный счетчик и проверяет, что получен только он
+ * Тест, который проверяет получение всех публичных счетчиков и проверяет их количество
  */
-public class GetEachGroupCounterTest extends ApiTest {
+public class GetAllGroupCountersAndValidateResponseTest extends ApiTest {
 
-    private static final Logger log = LoggerFactory.getLogger(GetEachGroupCounterTest.class);
+    private static final Logger log = LoggerFactory.getLogger(GetAllGroupCountersAndValidateResponseTest.class);
 
     private static final String GROUP_ID = "70000006977225";
+    private static final String ALL_COUNTER_TYPES = "BLACK_LIST, MEMBERS, MODERATORS, PHOTOS, PHOTO_ALBUMS, THEMES, LINKS, PRESENTS";
 
+    @Test
     @Tag("GET")
-    @DisplayName("Тест, который получает определенный счетчик и проверяет, что получен только он")
-    @ParameterizedTest
-    @ValueSource(strings = {"BLACK_LIST", "MEMBERS", "MODERATORS", "PHOTOS", "PHOTO_ALBUMS", "THEMES", "LINKS", "PRESENTS"})
-    public void getEachGroupCounterTest(String ContentType) {
+    @DisplayName("Тест, который проверяет получение всех публичных счетчиков и проверяет их количество")
+    public void getAllGroupCountersAndValidateResponseTest() {
         log.info("Получаем показатели группы");
         Map<String, Object> groupCounters = given()
                 .spec(requestSpec(BASE_URL))
                 .pathParam("application_key", APPLICATION_KEY)
-                .pathParam("counterTypes", ContentType)
+                .pathParam("counterTypes", ALL_COUNTER_TYPES)
                 .pathParam("format", "json")
                 .pathParam("group_id", GROUP_ID)
                 .pathParam("method", GroupMethodsUri.getGroupCounters)
@@ -47,10 +47,12 @@ public class GetEachGroupCounterTest extends ApiTest {
                 .spec(responseSpecOK200())
                 .log().all()
                 .extract().response().jsonPath().getMap("counters");
-        log.info("Проверяем, что тело ответа содержит требуемый элемент");
-        assertTrue(groupCounters.containsKey(ContentType.toLowerCase()));
-
         log.info("Проверяем, что тело ответа содержит только требуемый элемент");
-        assertEquals(1, groupCounters.size());
+        assertEquals(8, groupCounters.size());
+
+        log.info("Проверяем, что тело ответа содержит каждый запрошенный счетчик");
+        assertTrue(Arrays.stream(
+                ALL_COUNTER_TYPES.split(", "))
+                .allMatch(counter -> groupCounters.containsKey(counter.toLowerCase())));
     }
 }
---
+++ src/test/java/tests/group/get/positive/GetEachGroupCounterAndValidateResponseTest.java
@@ -0,0 +1,56 @@
+package tests.group.get.positive;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tests.ApiTest;
+import utils.Endpoints;
+import utils.GroupMethodsUri;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static utils.Specifications.requestSpec;
+import static utils.Specifications.responseSpecOK200;
+
+/**
+ * Тест, который получает определенный счетчик и проверяет, что получен только он
+ */
+public class GetEachGroupCounterAndValidateResponseTest extends ApiTest {
+
+    private static final Logger log = LoggerFactory.getLogger(GetEachGroupCounterAndValidateResponseTest.class);
+
+    private static final String GROUP_ID = "70000006977225";
+
+    @Tag("GET")
+    @DisplayName("Тест, который получает определенный счетчик и проверяет, что получен только он")
+    @ParameterizedTest
+    @ValueSource(strings = {"BLACK_LIST", "MEMBERS", "MODERATORS", "PHOTOS", "PHOTO_ALBUMS", "THEMES", "LINKS", "PRESENTS"})
+    public void getEachGroupCounterTest(String ContentType) {
+        log.info("Получаем показатели группы");
+        Map<String, Object> groupCounters = given()
+                .spec(requestSpec(BASE_URL))
+                .pathParam("application_key", APPLICATION_KEY)
+                .pathParam("counterTypes", ContentType)
+                .pathParam("format", "json")
+                .pathParam("group_id", GROUP_ID)
+                .pathParam("method", GroupMethodsUri.getGroupCounters)
+                .pathParam("sig", SIG)
+                .pathParam("access_token", ACCESS_TOKEN)
+                .get(Endpoints.getGroupCounters)
+                .then()
+                .spec(responseSpecOK200())
+                .log().all()
+                .extract().response().jsonPath().getMap("counters");
+        log.info("Проверяем, что тело ответа содержит требуемый элемент");
+        assertTrue(groupCounters.containsKey(ContentType.toLowerCase()));
+
+        log.info("Проверяем, что тело ответа содержит только требуемый элемент");
+        assertEquals(1, groupCounters.size());
+    }
+}
