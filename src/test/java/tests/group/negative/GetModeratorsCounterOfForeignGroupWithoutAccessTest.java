39d20a59868e8e7c173f5b2267f974ea4b3c9bfb
boris <borissiobko@gmail.com>
2024-07-22 03:02:38 +0300

    Add GetModeratorsCounterOfForeignGroupWithoutAccessTest

---
+++ src/test/java/tests/group/negative/GetModeratorsCounterOfForeignGroupWithoutAccessTest.java
@@ -0,0 +1,52 @@
+package tests.group.negative;
+
+import models.GroupCounterType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tests.ApiTest;
+import utils.Endpoints;
+import utils.GroupMethodsUri;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static utils.Specifications.requestSpec;
+import static utils.Specifications.responseSpecOK200;
+
+/**
+ * Тест, который проверяет неполучение счетчика moderators при отсутствии прав админа
+ */
+public class GetModeratorsCounterOfForeignGroupWithoutAccessTest extends ApiTest {
+
+    private static final Logger log = LoggerFactory.getLogger(GetModeratorsCounterOfForeignGroupWithoutAccessTest.class);
+
+    // ID группы, у которой скрыт счетчик moderators
+    private static final String GROUP_WITH_PRIVATE_MODERATORS_COUNTER = "51304450687182";
+
+    @Test
+    @Tag("group")
+    @Tag("negative")
+    @DisplayName("Тест, который проверяет неполучение счетчика moderators при отсутствии прав админа")
+    public void getModeratorsCounterOfForeignGroupWithoutAccessTest() {
+        log.info("Отправляем запрос на получение показателя moderators, не имея необходимых прав");
+        Map<String, Object> groupCounters = given()
+                .spec(requestSpec(BASE_URL))
+                .pathParam("application_key", APPLICATION_KEY)
+                .pathParam("counterTypes", GroupCounterType.MODERATORS)
+                .pathParam("format", "json")
+                .pathParam("group_id", GROUP_WITH_PRIVATE_MODERATORS_COUNTER)
+                .pathParam("method", GroupMethodsUri.getGroupCounters)
+                .pathParam("sig", SIG)
+                .pathParam("access_token", ACCESS_TOKEN)
+                .get(Endpoints.getGroupCounters)
+                .then()
+                .spec(responseSpecOK200())
+                .extract().response().jsonPath().getMap("counters");
+        log.info("Проверяем, что тело ответа не содержит moderators");
+        assertFalse(groupCounters.containsKey("moderators"));
+    }
+}
