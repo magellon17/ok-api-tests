33559463f49fb31d1cc2943628a5b208673ab8f4
boris <borissiobko@gmail.com>
2024-07-22 03:10:47 +0300

    Add class of error messages and update GetCountersOfNonExistentGroupAndValidateErrorMessageTest

---
+++ src/test/java/models/ErrorMessages.java
@@ -0,0 +1,9 @@
+package models;
+
+public enum ErrorMessages {
+    NOT_FOUND{
+        public String toString() {
+            return "NOT_FOUND : not.found.object";
+        }
+    }
+}
--- src/test/java/tests/group/negative/GetCountersOfNonExistentGroupAndValidateErrorMessageTest.java
+++ src/test/java/tests/group/negative/GetCountersOfNonExistentGroupAndValidateErrorMessageTest.java
@@ -1,5 +1,7 @@
 package tests.group.negative;
 
+import models.ErrorMessages;
+import models.GroupCounterType;
 import models.ResponseError;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -11,30 +13,31 @@ import utils.Endpoints;
 import utils.GroupMethodsUri;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static utils.Specifications.requestSpec;
 import static utils.Specifications.responseSpecOK200;
 
 /**
- * Тест, который проверяет получение всех публичных счетчиков чужой группы
+ * Тест, который проверяет получение ошибки при получении счетчика несуществующей группы
  */
-public class GetCountersOfNonExistentGroupTest extends ApiTest {
+public class GetCountersOfNonExistentGroupAndValidateErrorMessageTest extends ApiTest {
 
-    private static final Logger log = LoggerFactory.getLogger(GetCountersOfNonExistentGroupTest.class);
+    private static final Logger log = LoggerFactory.getLogger(GetCountersOfNonExistentGroupAndValidateErrorMessageTest.class);
 
-    private static final String INVALID_GROUP_ID = "70000006977222";
+    private static final String NON_EXISTENT_GROUP_ID = "70000006977222";
 
     @Test
     @Tag("group")
     @Tag("negative")
     @DisplayName("Тест, который получает определенный счетчик собственной группы и проверяет, что получен только он")
-    public void getInvalidGroupCountersTest() {
+    public void getCountersOfNonExistentGroupAndValidateErrorMessageTest() {
         log.info("Получаем показатели несуществующей группы");
         ResponseError error = given()
                 .spec(requestSpec(BASE_URL))
                 .pathParam("application_key", APPLICATION_KEY)
-                .pathParam("counterTypes", "BLACK_LIST")
+                .pathParam("counterTypes", GroupCounterType.MEMBERS)
                 .pathParam("format", "json")
-                .pathParam("group_id", INVALID_GROUP_ID)
+                .pathParam("group_id", NON_EXISTENT_GROUP_ID)
                 .pathParam("method", GroupMethodsUri.getGroupCounters)
                 .pathParam("sig", SIG)
                 .pathParam("access_token", ACCESS_TOKEN)
@@ -42,7 +45,8 @@ public class GetCountersOfNonExistentGroupTest extends ApiTest {
                 .then()
                 .spec(responseSpecOK200())
                 .extract().as(ResponseError.class);
-        log.info("Проверяем, что тело ответа содержит ошибку");
-        // дальше проверка, но я уже не успеваю (((((((
+        log.info("Проверяем, что тело ответа содержит правильное сообщение об ошибке");
+        assertEquals(error.getError_msg(), ErrorMessages.NOT_FOUND.toString(),
+                "Сообщение об NOT_FOUND ошибке не совпало с требуемым");
     }
 }
