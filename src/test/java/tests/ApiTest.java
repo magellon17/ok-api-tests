671d5354d5950f1782405e42b8bf26bcdf74b2d6
boris <borissiobko@gmail.com>
2024-07-25 22:34:13 +0300

    Updated access token

--- src/test/java/tests/ApiTest.java
+++ src/test/java/tests/ApiTest.java
@@ -2,7 +2,7 @@ package tests;
 
 public class ApiTest {
     protected final String BASE_URL = "https://api.ok.ru";
-    protected final String ACCESS_TOKEN = "-s-dVeoGXrN4Q-IGyHsXWBmHSlw1QCCrTJJ2RDJGsLO00gDLXpJ4P8ns-oJ0zcopWlK2.gDoyHK3VeFGViM0VdGtvo6";
+    protected final String ACCESS_TOKEN = "-s-6Q9Br.lL.QDGqxMQ00gDG0qLXPAnHTiNzQbDtXhta1iHIvkN0SbEGxnu1SA-MzIM0SCDqSpO4.CJKUpO6Rbq-v8";
     protected final String SIG = "f4384ed7f3b19a5e779db21b0d4c6516";
     protected final String APPLICATION_KEY = "CPCNFMLGDIHBABABA";
 }
